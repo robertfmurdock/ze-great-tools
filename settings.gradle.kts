406095d6169858217b6fab080d4733070dafa580
RoB Murdock <robert.f.murdock@gmail.com>
2023-02-22 20:25:36 -0500

    enabling gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    `gradle-enterprise`
+}
+
 rootProject.name = "ze-great-tools"
 include("lib")
 includeBuild("tagger")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
