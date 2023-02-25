072fbb88471c2f98493a0a1ae6b7e5e8c402cf29
RoB Murdock <robert.f.murdock@gmail.com>
2023-02-24 22:38:53 -0500

    removing publishing from root level, because there's nothing to publish there

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,7 @@ plugins {
     id("com.zegreatrob.tools.tagger")
     id("com.zegreatrob.tools.plugins.lint")
     id("com.zegreatrob.tools.plugins.versioning")
-    `maven-publish`
-    signing
-    alias(libs.plugins.com.github.ben.manes.versions)
     alias(libs.plugins.nl.littlerobots.version.catalog.update)
-    alias(libs.plugins.io.github.gradle.nexus.publish.plugin)
     base
 }
 
@@ -26,18 +22,6 @@ tasks {
     }
     release {
         dependsOn(provider { gradle.includedBuild("tagger").task(":release") })
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            username.set(System.getenv("SONATYPE_USERNAME"))
-            password.set(System.getenv("SONATYPE_PASSWORD"))
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            stagingProfileId.set("59331990bed4c")
-        }
     }
 }
 
