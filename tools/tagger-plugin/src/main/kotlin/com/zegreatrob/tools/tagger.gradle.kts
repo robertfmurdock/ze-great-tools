29d509f6dcde443dc76154a36dba77b3b116da47
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-22 00:34:07 -0400

    [patch] removing extra =s

--- tools/tagger-plugin/src/main/kotlin/com/zegreatrob/tools/tagger.gradle.kts
+++ tools/tagger-plugin/src/main/kotlin/com/zegreatrob/tools/tagger.gradle.kts
@@ -53,7 +53,7 @@ tasks {
         dependsOn(tag)
         val githubRepository = System.getenv("GITHUB_REPOSITORY")
         commandLine(
-            "gh api --method POST -H \"Accept: application/vnd.github+json\" -H \"X-GitHub-Api-Version: 2022-11-28\" /repos/$githubRepository/releases  -f=tag_name='${tagger.version}' -f=name='${tagger.version}' -f=body='${tagger.version} -F draft=false -F prerelease=false -F generate_release_notes=false"
+            "gh api --method POST -H \"Accept: application/vnd.github+json\" -H \"X-GitHub-Api-Version: 2022-11-28\" /repos/$githubRepository/releases  -f tag_name='${tagger.version}' -f name='${tagger.version}' -f body='${tagger.version} -F draft=false -F prerelease=false -F generate_release_notes=false"
                 .split(" "),
         )
     }
