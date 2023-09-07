21a9b7f664559a9314073d448c335c8d4bdef8c8
RoB Murdock <rob@zegreatrob.com>
2023-09-07 17:18:48 -0400

    [patch] a lil more logic moved to core

---
+++ tools/digger-core/src/main/kotlin/com/zegreatrob/tools/digger/core/CurrentContributionCommits.kt
@@ -0,0 +1,25 @@
+package com.zegreatrob.tools.digger.core
+
+import org.ajoberstar.grgit.Commit
+import org.ajoberstar.grgit.Grgit
+import org.ajoberstar.grgit.Tag
+import org.ajoberstar.grgit.operation.LogOp
+
+fun Grgit.currentContributionCommits(): List<Commit> {
+    val tag = previousTag()
+    return if (tag == null) {
+        log()
+    } else {
+        return log(fun(it: LogOp) { it.range(tag, "HEAD") })
+    }
+}
+
+private fun Grgit.previousTag(): Tag? {
+    val tagList = tag.list().sortedByDescending { it.dateTime }
+    val tag = tagList.firstOrNull()
+    return if (tag?.commit == head()) {
+        tagList.getOrNull(1)
+    } else {
+        tag
+    }
+}
--- tools/digger-plugin/src/main/kotlin/com/zegreatrob/tools/digger/DiggerExtension.kt
+++ tools/digger-plugin/src/main/kotlin/com/zegreatrob/tools/digger/DiggerExtension.kt
@@ -2,11 +2,8 @@ package com.zegreatrob.tools.digger
 
 import com.zegreatrob.tools.digger.core.allContributionCommits
 import com.zegreatrob.tools.digger.core.contributionDataJson
-import org.ajoberstar.grgit.Commit
-import org.ajoberstar.grgit.Grgit
-import org.ajoberstar.grgit.Tag
+import com.zegreatrob.tools.digger.core.currentContributionCommits
 import org.ajoberstar.grgit.gradle.GrgitServiceExtension
-import org.ajoberstar.grgit.operation.LogOp
 
 open class DiggerExtension(
     private val grgitServiceExtension: GrgitServiceExtension,
@@ -19,25 +16,6 @@ open class DiggerExtension(
     fun currentContributionData() = grgitServiceExtension.service.get().grgit
         .currentContributionCommits()
         .contributionDataJson()
-
-    private fun Grgit.currentContributionCommits(): List<Commit> {
-        val tag = previousTag()
-        return if (tag == null) {
-            log()
-        } else {
-            return log(fun(it: LogOp) { it.range(tag, "HEAD") })
-        }
-    }
-
-    private fun Grgit.previousTag(): Tag? {
-        val tagList = tag.list().sortedByDescending { it.dateTime }
-        val tag = tagList.firstOrNull()
-        return if (tag?.commit == head()) {
-            tagList.getOrNull(1)
-        } else {
-            tag
-        }
-    }
 
     fun headId(): String = grgitServiceExtension.service.get().grgit.head().id
 }
