2e16bbbcc173a0e8f2e1ea50072bdb386d31dabe
RoB Murdock <rob@zegreatrob.com>
2024-06-02 12:06:42 -0400

    [patch] updating gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,17 +6,17 @@ pluginManagement {
 }
 
 plugins {
-    `gradle-enterprise`
+    id("com.gradle.develocity") version "3.17.3"
 }
 
 rootProject.name = "ze-great-tools"
 includeBuild("tools-plugins")
 includeBuild("tools")
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
     }
 }
 
