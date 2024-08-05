e8c4376232a61c0f040a1697a191e3cb1802e98a
RoB Murdock <rob@zegreatrob.com>
2024-08-05 16:51:18 -0400

    [patch] supporting multiple merges from same branch

--- tools/digger-core/src/commonMain/kotlin/com/zegreatrob/tools/digger/core/AllContributionCommits.kt
+++ tools/digger-core/src/commonMain/kotlin/com/zegreatrob/tools/digger/core/AllContributionCommits.kt
@@ -3,42 +3,62 @@ package com.zegreatrob.tools.digger.core
 fun DiggerGitWrapper.allContributionCommits(): List<Pair<TagRef?, List<CommitRef>>> {
     val tagList = listTags()
     val log = log()
+    val mainBranchLog = log.mainBranch()
+    val mainBranchLogsWithFoldedBranches = mainBranchLog.foldInBranches(log)
+    return tagList.relatedCommitIds(mainBranchLogsWithFoldedBranches)
+        .withCommitsInOriginalOrder(log)
+}
 
-    val mainBranchLog = log.fold(emptyList<CommitRef>()) { acc, commit ->
-        if (acc.isEmpty()) {
-            acc + commit
-        } else if (commit.id == acc.last().parents.first()) {
-            acc + commit
-        } else {
-            acc
-        }
-    }
-
-    val offMainCommits = log - mainBranchLog.toSet()
-
-    val mainBranchLogsWithFoldedBranches = mainBranchLog.flatMap { commit ->
-
-        if (commit.parents.size > 1) {
-            listOf(commit) + branchCommits(commit.parents[1], offMainCommits)
-        } else {
-            listOf(commit)
-        }
-    }
-
-    return mainBranchLogsWithFoldedBranches.fold(emptyList()) { acc, commit ->
-        val tag = tagList.find { it.commitId == commit.id }
+private fun List<TagRef>.relatedCommitIds(
+    mainBranchLogsWithFoldedBranches: List<CommitRef>,
+): List<Pair<TagRef?, Set<String>>> {
+    val tagSets: List<Pair<TagRef?, Set<String>>> = mainBranchLogsWithFoldedBranches.fold(emptyList()) { acc, commit ->
+        val tag = find { it.commitId == commit.id }
         if (tag != null) {
-            acc.plus(element = tag to listOf(commit))
+            acc.plus(element = tag to setOf(commit.id))
         } else {
             val lastPair = acc.lastOrNull()
             val lastList = lastPair?.second
             if (lastList != null) {
-                acc.slice(0..acc.size - 2).plus(element = lastPair.copy(second = lastList.plusElement(commit)))
+                acc.slice(0..acc.size - 2).plus(element = lastPair.copy(second = lastList.plusElement(commit.id)))
             } else {
-                acc.plus(element = null to listOf(commit))
+                acc.plus(element = null to setOf(commit.id))
             }
         }
     }
+    return tagSets
+}
+
+private fun List<CommitRef>.mainBranch() = fold(emptyList<CommitRef>()) { acc, commit ->
+    if (acc.isEmpty()) {
+        acc + commit
+    } else if (commit.id == acc.last().parents.first()) {
+        acc + commit
+    } else {
+        acc
+    }
+}
+
+private fun List<CommitRef>.foldInBranches(log: List<CommitRef>): List<CommitRef> {
+    val offMainCommits = log - toSet()
+    return reversed()
+        .fold<CommitRef, List<CommitRef>>(emptyList()) { acc, commit ->
+            if (commit.parents.size > 1) {
+                acc + branchCommits(
+                    commit.parents[1],
+                    offMainCommits - acc.toSet(),
+                ) + listOf(commit)
+            } else {
+                acc + listOf(commit)
+            }
+        }
+        .reversed()
+}
+
+private fun List<Pair<TagRef?, Set<String>>>.withCommitsInOriginalOrder(
+    log: List<CommitRef>,
+) = map { (tag, commitIds) ->
+    tag to log.filter { commit -> commitIds.contains(commit.id) }
 }
 
 fun branchCommits(id: String?, offMainCommits: List<CommitRef>): List<CommitRef> = (0..offMainCommits.size)
--- tools/digger-test/src/jvmMain/kotlin/com/zegreatrob/tools/digger/AllContributionTestSpec.kt
+++ tools/digger-test/src/jvmMain/kotlin/com/zegreatrob/tools/digger/AllContributionTestSpec.kt
@@ -127,6 +127,70 @@ interface AllContributionTestSpec : SetupWithOverrides {
         )
     }
 
+    @Test
+    fun `when merging multiple times from same branch, commits are only counted once`() {
+        setupWithDefaults()
+        val grgit = initializeGitRepo(
+            projectDirectoryPath = projectDir.absolutePath,
+            addFileNames = addFileNames,
+            listOf("first"),
+        )
+        val firstCommit = grgit.head()
+
+        val firstRelease = grgit.addTag("release")
+        grgit.branch.add { it.name = "branch1" }
+        grgit.checkout { it.branch = "branch1" }
+        val secondCommit = grgit.addCommitWithMessage("second")
+        grgit.checkout { it.branch = "main" }
+        val thirdCommit = grgit.addCommitWithMessage("third")
+        grgit.merge {
+            it.head = "branch1"
+            it.setMode("no-commit")
+        }
+        val merge1Commit = grgit.addCommitWithMessage("merge1")
+        val secondRelease = grgit.addTag("release2")
+
+        grgit.checkout { it.branch = "branch1" }
+        val fourthCommit = grgit.addCommitWithMessage("fourth")
+        grgit.checkout { it.branch = "main" }
+        grgit.merge {
+            it.head = "branch1"
+            it.setMode("no-commit")
+        }
+        val merge2Commit = grgit.addCommitWithMessage("merge2")
+        val thirdRelease = grgit.addTag("release3")
+
+        val allOutput = runAllContributionData()
+        val expectedAuthors = listOf(
+            "funk@test.io",
+            "test@funk.edu",
+        )
+        assertEquals(
+            listOf(
+                toContribution(
+                    lastCommit = merge2Commit,
+                    firstCommit = fourthCommit,
+                    expectedCommitCount = 2,
+                    tag = thirdRelease,
+                    expectedAuthors = expectedAuthors,
+                ),
+                toContribution(
+                    lastCommit = merge1Commit,
+                    firstCommit = secondCommit,
+                    expectedCommitCount = 3,
+                    tag = secondRelease,
+                    expectedAuthors = expectedAuthors,
+                ),
+                toContribution(
+                    lastCommit = firstCommit,
+                    tag = firstRelease,
+                    expectedAuthors = expectedAuthors,
+                ),
+            ),
+            parseAll(allOutput),
+        )
+    }
+
     private fun toContribution(
         lastCommit: Commit,
         tag: Tag? = null,
