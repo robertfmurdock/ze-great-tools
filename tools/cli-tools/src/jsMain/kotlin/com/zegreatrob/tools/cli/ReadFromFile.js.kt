dd299e23362272648028929083681df3d9fb70f6
RoB Murdock <rob@zegreatrob.com>
2024-09-10 12:49:24 -0400

    [patch] fixing js config file error

--- tools/cli-tools/src/jsMain/kotlin/com/zegreatrob/tools/cli/ReadFromFile.js.kt
+++ tools/cli-tools/src/jsMain/kotlin/com/zegreatrob/tools/cli/ReadFromFile.js.kt
@@ -3,6 +3,9 @@ package com.zegreatrob.tools.cli
 import kotlin.js.json
 
 private val fs = js("require('node:fs')")
-actual fun readFromFile(fileName: String): String? =
+actual fun readFromFile(fileName: String): String? = if (fs.existsSync(fileName).unsafeCast<Boolean>()) {
     fs.readFileSync(fileName, json("encoding" to "utf-8"))
         .unsafeCast<String>()
+} else {
+    null
+}
