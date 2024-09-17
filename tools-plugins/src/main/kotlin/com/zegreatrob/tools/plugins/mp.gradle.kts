08766554330e5767d512e90394d7628d35315a65
RoB Murdock <rob@zegreatrob.com>
2024-09-17 13:00:23 -0400

    [patch] enabling warnings as errors and fixing warnings

--- tools-plugins/src/main/kotlin/com/zegreatrob/tools/plugins/mp.gradle.kts
+++ tools-plugins/src/main/kotlin/com/zegreatrob/tools/plugins/mp.gradle.kts
@@ -1,5 +1,7 @@
 package com.zegreatrob.tools.plugins
 
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
     base
     id("com.zegreatrob.tools.plugins.reports")
@@ -12,6 +14,11 @@ plugins {
 
 kotlin {
     jvmToolchain(11)
+
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
 }
 
 tasks {
