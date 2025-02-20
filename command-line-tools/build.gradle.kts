4341ade1e422852d753af838c5c65537a0c0e56e
RoB Murdock <rob@zegreatrob.com>
2025-02-19 22:34:54 -0500

    [patch] minor build tweaks and forcing a new version after the last partial release

--- command-line-tools/build.gradle.kts
+++ command-line-tools/build.gradle.kts
@@ -42,7 +42,7 @@ tasks {
         revision = "release"
 
         rejectVersionIf {
-            "^[0-9.]+[0-9](-RC|-M[0-9]+|-RC[0-9]+|-beta.*|-Beta.*|-alpha.*)\$"
+            "^[0-9.]+[0-9](-RC|-M[0-9]+|-RC[0-9]+|-beta.*|-Beta.*|-alpha.*)$"
                 .toRegex(RegexOption.IGNORE_CASE)
                 .matches(candidate.version)
         }
