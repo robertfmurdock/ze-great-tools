b5ea8d3feeb710b9b77f81c959003b3489435558
RoB Murdock <rob@zegreatrob.com>
2026-02-16 18:31:48 -0500

    [patch] using compare task in build github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ tagger {
 
 fingerprintConfig {
     includedBuilds = listOf("command-line-tools", "tools")
+    compareToFingerprintFile = layout.buildDirectory.file("prev-aggregate-fingerprint.txt")
 }
 
 tasks {
