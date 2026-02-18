64325b6892ce942cb65662b1f6ab33a7c006a6ac
RoB Murdock <rob@zegreatrob.com>
2026-02-17 21:25:13 -0500

    [patch] converting another project to use testmints

--- tools/git-adapter/src/jvmTest/kotlin/com/zegreatrob/tools/adapter/git/GitAdapterTest.kt
+++ tools/git-adapter/src/jvmTest/kotlin/com/zegreatrob/tools/adapter/git/GitAdapterTest.kt
@@ -1,45 +1,41 @@
 package com.zegreatrob.tools.adapter.git
 
+import com.zegreatrob.testmints.async.asyncSetup
 import com.zegreatrob.tools.test.git.addCommitWithMessage
 import com.zegreatrob.tools.test.git.delayLongEnoughToAffectGitDate
 import com.zegreatrob.tools.test.git.initializeGitRepo
-import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 class GitAdapterTest {
 
     @field:TempDir
     lateinit var projectDir: File
-    private lateinit var wrapper: GitAdapter
-
-    @BeforeTest
-    fun setup() {
-        wrapper = GitAdapter(projectDir.absolutePath)
-    }
-
-    fun initializeGitRepo(commits: List<String>) = initializeGitRepo(
-        directory = projectDir.absolutePath,
-        addFileNames = emptySet(),
-        commits = commits,
-    )
 
     @Test
-    fun `will include all tag segments from newest to oldest`() = runTest {
-        val gitAdapter = initializeGitRepo(commits = listOf("here's a message"))
-        gitAdapter.config("user.name", "Test")
-        gitAdapter.config("user.email", "Test")
-        gitAdapter.newAnnotatedTag("v1.0", "HEAD", null, null)
+    fun `will include all tag segments from newest to oldest`() = asyncSetup(object {
+        val wrapper = GitAdapter(projectDir.absolutePath)
+    }) {
+        initializeGitRepo(
+            directory = projectDir.absolutePath,
+            addFileNames = emptySet(),
+            commits = listOf("here's a message"),
+        ).apply {
+            config("user.name", "Test")
+            config("user.email", "Test")
+            newAnnotatedTag("v1.0", "HEAD", null, null)
+        }
         delayLongEnoughToAffectGitDate()
-        gitAdapter.addCommitWithMessage("here's a message")
-        gitAdapter.newAnnotatedTag("1.10", "HEAD", null, null)
+        wrapper.addCommitWithMessage("here's a message")
+        wrapper.newAnnotatedTag("1.10", "HEAD", null, null)
         delayLongEnoughToAffectGitDate()
-        gitAdapter.addCommitWithMessage("here's a message")
-        gitAdapter.newAnnotatedTag("1.101", "HEAD", null, null)
-
-        assertEquals(listOf("1.101", "1.10", "v1.0"), wrapper.listTags().map { it.name })
+        wrapper.addCommitWithMessage("here's a message")
+        wrapper.newAnnotatedTag("1.101", "HEAD", null, null)
+    } exercise {
+        wrapper.listTags()
+    } verify { result ->
+        assertEquals(listOf("1.101", "1.10", "v1.0"), result.map { it.name })
     }
 }
