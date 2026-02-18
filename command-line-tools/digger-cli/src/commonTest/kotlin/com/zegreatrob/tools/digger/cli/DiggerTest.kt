07c21270f71db5d61cacecdd6daeb8dcac90bbe4
RoB Murdock <rob@zegreatrob.com>
2026-02-17 21:54:37 -0500

    [patch] converting one of the cli tools tests to use testmints

--- command-line-tools/digger-cli/src/commonTest/kotlin/com/zegreatrob/tools/digger/cli/DiggerTest.kt
+++ command-line-tools/digger-cli/src/commonTest/kotlin/com/zegreatrob/tools/digger/cli/DiggerTest.kt
@@ -1,6 +1,7 @@
 package com.zegreatrob.tools.digger.cli
 
 import com.github.ajalt.clikt.testing.test
+import com.zegreatrob.testmints.setup
 import com.zegreatrob.tools.test.git.getEnvironmentVariable
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -9,15 +10,13 @@ import kotlin.test.assertNotNull
 class DiggerTest {
 
     @Test
-    fun versionWillReturnAppropriateVersion() {
+    fun versionWillReturnAppropriateVersion() = setup(object {
         val expectedVersion = getEnvironmentVariable("EXPECTED_VERSION")
+    }) {
         assertNotNull(expectedVersion, "Test not setup correctly - include build version")
-        cli()
-            .test("--version")
-            .output
-            .trim()
-            .let {
-                assertEquals("digger version $expectedVersion", it)
-            }
+    } exercise {
+        cli().test("--version")
+    } verify { result ->
+        assertEquals("digger version $expectedVersion", result.output.trim())
     }
 }
