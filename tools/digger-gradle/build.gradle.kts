5a339f1146d85c6d64ff9171093463f111e346db
RoB Murdock <rob@zegreatrob.com>
2026-02-21 19:10:29 -0500

    [patch] fixing issue with missing publication information for intermediate dependency

--- tools/digger-gradle/build.gradle.kts
+++ tools/digger-gradle/build.gradle.kts
@@ -1,10 +1,29 @@
 plugins {
     id("com.zegreatrob.tools.plugins.jvm")
+    id("com.zegreatrob.tools.plugins.publish")
     `java-library`
+}
+
+java {
+    withSourcesJar()
 }
 
 dependencies {
     api(project(":digger-core"))
     api(project(":digger-json"))
     api(project(":git-adapter"))
+}
+
+tasks {
+    val javadocJar by registering(Jar::class) {
+        archiveClassifier.set("javadoc")
+        from("${rootDir.absolutePath}/javadocs")
+    }
+
+    publishing.publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(javadocJar)
+        }
+    }
 }
