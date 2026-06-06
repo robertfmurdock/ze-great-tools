5a1135698ccdcd38443a2eb988025d2c0993f566
RoB Murdock <rob@zegreatrob.com>
2026-06-06 00:29:18 -0400

    [none] removing redundant guide tests

--- tools/digger-guide/build.gradle.kts
+++ tools/digger-guide/build.gradle.kts
@@ -24,13 +24,6 @@ kotlin {
     }
 }
 
-dependencies {
-    commonTestImplementation(platform(libs.com.zegreatrob.testmints.bom))
-    commonTestImplementation(kotlin("test"))
-    commonTestImplementation(libs.com.zegreatrob.testmints.minassert)
-    commonTestImplementation(libs.com.zegreatrob.testmints.standard)
-}
-
 tasks {
     withType(Test::class) {
         useJUnitPlatform()
